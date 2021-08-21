9798bd76cc2a976c3cf5b630090d8f6fa842213c
Stepania <super.kydryavuy2011@yandex.ru>
2021-08-21 15:04:59 +1200

    querries1,2

---
+++ hospitalSQL/querries.sql
@@ -0,0 +1,25 @@
+USE HOSPITAL;
+GO
+--1 querry how many parients cardio....something
+SELECT COUNT(*) as refferedToCardio
+FROM REFERRAL
+WHERE departmentID = 2
+
+
+----second querry
+
+
+SELECT DATEDIFF(DAY,addedDate,FSADate) AS timeTaken, departmentName AS departmentName
+INTO waitingTimeBeforeSeen
+FROM WAITLIST 
+JOIN DEPARTMENT ON Waitlist.departmentID=Department.departmentID
+WHERE NOT FSADate = '2015-11-11'
+;
+SELECT * FROM waitingTimeBeforeSeen
+
+SELECT departmentName, AVG(timeTaken) AS [AvgTimeTaken]
+FROM waitingTimeBeforeSeen
+GROUP BY departmentName
+ORDER BY departmentName
+
+DROP TABLE waitingTimeBeforeSeen
